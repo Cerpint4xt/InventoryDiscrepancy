587afde1cc9142f9666470144276a29e5c67dc79
Cerpint4xt <rcatagrundy@gmail.com>
2022-08-20 19:44:24 -0400

    trying spinner

--- inventory_discrepancy_st.py
+++ inventory_discrepancy_st.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 
 
-st.title('Inventory Discrepancy')
+st.title(':bar_chart: Inventory Discrepancy')
 
 DATA_URL_expected = ("https://storage.googleapis.com/mojix-devops-wildfire-bucket/analytics/bootcamp_2_0/Bootcamp_DataAnalysis_Expected.csv")
 
@@ -14,14 +14,24 @@ def load_data(url):
     data = pd.read_csv(url, encoding="latin-1", dtype=str)
     return data
 
+with st.spinner(text='In progress'):
+    data_expected = load_data(DATA_URL_expected)
+    time.sleep(5)
+    st.success("Done! df_expected loaded!")
+
+with st.spinner(text='In progress'):
+    data_expected = load_data(DATA_URL_counted)
+    time.sleep(5)
+    st.success("Done! df_expected loaded!")
+'''
 data_load_state = st.text('Loading data...')
-data_expected = load_data(DATA_URL_expected)
+
 data_load_state.text("Done! df_expected loaded! (using st.cache)")
 
 data_load_state = st.text('Loading data...')
 data_counted = load_data(DATA_URL_counted)
 data_load_state.text("Done! df_counted loaded! (using st.cache)")
-
+'''
 if st.checkbox('Show raw data expected'):
     st.subheader('Raw data expected')
     st.write(data_expected)
@@ -29,3 +39,5 @@ if st.checkbox('Show raw data expected'):
 if st.checkbox('Show raw data counted'):
     st.subheader('Raw data counted')
     st.write(data_counted)
+
+st.markdown('---')
