a4c1a8a426993b92d92b69782395ae06f64ca2d6
Cerpint4xt <rcatagrundy@gmail.com>
2022-08-20 21:07:41 -0400

    df enriched for visualization

--- inventory_discrepancy_st.py
+++ inventory_discrepancy_st.py
@@ -29,3 +29,31 @@ if st.checkbox('Show raw data counted'):
     st.write(data_counted)
 
 st.markdown('---')
+
+df_counted = data_counted
+df_counted = df_counted.drop_duplicates('RFID')
+df_B = df_counted.groupby("Retail_Product_SKU").count()[["RFID"]].reset_index().rename(columns={"RFID":"Retail_CCQTY"})
+
+my_col_selected = ["Retail_Product_Name",
+                   "Retail_SOHQTY"]
+df_A = df_expected[my_cols_selected]
+
+
+df_discrepancy = pd.merge(df_A, df_B, how="outer", left_on="Retail_Product_SKU", right_on="Retail_Product_SKU", indicator=True)
+df_discrepancy['Retail_CCQTY'] = df_discrepancy['Retail_CCQTY'].fillna(0).astype(int)
+df_discrepancy["Retail_SOHQTY"] = df_discrepancy["Retail_SOHQTY"].fillna(0).astype(int)
+
+df_discrepancy.loc[df_discrepancy["Retail_SOHQTY"] == df_discrepancy["Retail_CCQTY"], "Match"] = 1
+df_discrepancy["Match"] = df_discrepancy["Match"].fillna(0).astype(int)
+df_discrepancy["Diff"] = df_discrepancy["Retail_CCQTY"] - df_discrepancy["Retail_SOHQTY"]
+df_discrepancy.loc[df_discrepancy["Diff"]<0, "Unders"] = df_discrepancy["Diff"] * (-1)
+df_discrepancy["Unders"] = df_discrepancy["Unders"].fillna(0).astype(int)
+df_discrepancy.loc[df_discrepancy["Diff"]>0, "Overs"] = df_discrepancy["Diff"]
+df_discrepancy["Overs"] = df_discrepancy["Overs"].fillna(0).astype(int)
+df_discrepancy_enriched = df_discrepancy.groupby("Retail_Product_Name").sum()
+
+
+if st.checkbox('Show data frame Discrepancy'):
+    st.subheader('Data discrepancy with Match, Unders, Overs')
+    st.write(df_discrepancy_enriched)
+
