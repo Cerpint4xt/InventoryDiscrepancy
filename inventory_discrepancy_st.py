b71d26fb8a146cc253cff2345705d986a68918f8
Cerpint4xt <rcatagrundy@gmail.com>
2022-08-20 23:04:44 -0400

    again more graphics

--- inventory_discrepancy_st.py
+++ inventory_discrepancy_st.py
@@ -92,7 +92,7 @@ df_discrepancy_plot = df_discrepancy.groupby("Retail_Product_Name").sum()
 df_discrepancy_plot = df_discrepancy_plot[['Diff','Unders', 'Overs', 'Match']]
 st.bar_chart(df_discrepancy_plot)
 
-st. bar_chart(f_discrepancy_plot.plot.barh(stacked=True))
+st.bar_chart(f_discrepancy_plot.plot.barh(stacked=True))
 
 
 
